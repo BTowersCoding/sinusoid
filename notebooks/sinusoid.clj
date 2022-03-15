373d8353e742c73de3a24acf241122ca4c29dc1a
Bobby Towers <porkostomus@mail.com>
2022-03-14 20:56:13 -0700

    break down sinusoidal equations

--- notebooks/sinusoid.clj
+++ notebooks/sinusoid.clj
@@ -5,7 +5,13 @@
 
 ;; ## [Solve sinusoidal equations](https://www.khanacademy.org/math/trigonometry/trig-equations-and-identities/xfefa5515:sinusoidal-equations/e/solve-advanced-sinusoidal-equations)
 
-;; Find the solutions to the equation. Your answer should be in degrees. Assume $n$ is any integer.
+;; Find the solutions to the equation. Assume $n$ is any integer.
+
+(def mode :deg)
+
+^{:nextjournal.clerk/visibility #{:hide}}
+(case mode
+  :deg (clerk/html "Your answer should be in degrees rounded to the nearest hundredth."))
 
 ;; $7\cos(5x)+9=12$
 
@@ -18,9 +24,16 @@
 
 (def amplitude 7)
 (def trig-fn "\\cos")
+
 (def period 5)
 (def x-shift 9)
 (def equals 12)
+
+;; Trig identities
+^{:nextjournal.clerk/visibility #{:hide}}
+(clerk/tex "\\cos(\\theta)=\\cos(-\\theta)")
+^{:nextjournal.clerk/visibility #{:hide}}
+(clerk/tex "\\cos(\\theta)=\\cos(\\theta+360\\degree)")
 
 (defn inverse-trig-fn [n]
   (case trig-fn
@@ -36,19 +49,54 @@
       trig-fn "(" period "x)&=" (ratio (/ (- equals x-shift) amplitude))
       "\\end{aligned}"))
 
+;; Use the inverse trig function to find one value of `(* period x)`
+
 ^{:nextjournal.clerk/visibility #{:hide}}
 (clerk/tex
  (str trig-fn "^{-1}\\left(" (ratio (/ (- equals x-shift) amplitude)) "\\right)="
                   (deg (inverse-trig-fn (/ (- equals x-shift) amplitude))) "^\\circ"))
 
+;; Now we can use the first identity to find the second solution within the interval:
+
+^{:nextjournal.clerk/visibility #{:hide}}
+(clerk/tex (str "-" (deg (inverse-trig-fn (/ (- equals x-shift) amplitude))) " ^\\circ")) 
+
+;; Using the second identity, we can find all the solutions to our equation from the two angles we found above. The first solution gives us the following:
+
 ^{:nextjournal.clerk/visibility #{:hide}}
 (clerk/tex
  (str
-  "\\begin{aligned}5x&=64.62^\\circ+n\\cdot360^\\circ \\\\\\\\"
-  "x&=\\dfrac{64.62^\\circ+n\\cdot360^\\circ}{5}=12.92^\\circ+n\\cdot72^\\circ\\end{aligned}"))
+  "\\begin{aligned}"
+  period "x&="
+  (case mode
+    :deg (str (deg (inverse-trig-fn (/ (- equals x-shift) amplitude))) "^\\circ"))
+  "+n\\cdot360^\\circ \\\\\\\\"
+  "x&=\\dfrac{" 
+  (str (deg (inverse-trig-fn (/ (- equals x-shift) amplitude))) "^\\circ")
+  "+n\\cdot360^\\circ}{"
+  period
+  "}=" 
+  (/ (Math/round (* 100 (/ (deg (inverse-trig-fn (/ (- equals x-shift) amplitude)))
+                           period))) 
+     100.0)
+  "^\\circ+n\\cdot72^\\circ\\end{aligned}"))
+
+;; Similarly, the second solution gives us the following:
 
 ^{:nextjournal.clerk/visibility #{:hide}}
-(clerk/tex 
+(clerk/tex
  (str
-  "\\begin{aligned}5x&=-64.62^\\circ+n\\cdot360^\\circ \\\\\\\\"
-  "x&=\\dfrac{-64.62^\\circ+n\\cdot360^\\circ}{5}=-12.92^\\circ+n\\cdot72^\\circ\\end{aligned}"))
+  "\\begin{aligned}"
+  period "x&=-"
+  (case mode
+    :deg (str (deg (inverse-trig-fn (/ (- equals x-shift) amplitude))) "^\\circ"))
+  "+n\\cdot360^\\circ \\\\\\\\"
+  "x&=\\dfrac{-"
+  (str (deg (inverse-trig-fn (/ (- equals x-shift) amplitude))) "^\\circ")
+  "+n\\cdot360^\\circ}{"
+  period
+  "}=-"
+  (/ (Math/round (* 100 (/ (deg (inverse-trig-fn (/ (- equals x-shift) amplitude)))
+                           period)))
+     100.0)
+  "^\\circ+n\\cdot72^\\circ\\end{aligned}"))
