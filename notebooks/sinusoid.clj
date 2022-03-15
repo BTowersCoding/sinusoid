7e6f572828b828afb365044545a3a3d5450bdc6d
Bobby Towers <porkostomus@mail.com>
2022-03-14 22:36:58 -0700

    solve sin and cosine

--- notebooks/sinusoid.clj
+++ notebooks/sinusoid.clj
@@ -13,31 +13,37 @@
 (case mode
   :deg (clerk/html "Your answer should be in degrees rounded to the nearest hundredth."))
 
-;; $7\cos(5x)+9=12$
+;; $5\sin(3x)-1=3$
 
 (def pi Math/PI)
 
 (defn deg [rad]
   (/ (Math/round (* 100 (/ rad (/ pi 180)))) 100.0))
 
-(deg (Math/acos (/ 3 7)))
+(def amplitude 5)
+(def trig-fn "\\sin")
 
-(def amplitude 7)
-(def trig-fn "\\cos")
-
-(def period 5)
-(def x-shift 9)
-(def equals 12)
+(def period 3)
+(def x-shift -1)
+(def equals 3)
 
 ;; Trig identities
 ^{:nextjournal.clerk/visibility #{:hide}}
-(clerk/tex "\\cos(\\theta)=\\cos(-\\theta)")
+(clerk/tex 
+ (case trig-fn
+   "\\cos" "\\cos(\\theta)=\\cos(-\\theta)"
+   "\\sin" "\\sin(\\theta)=\\sin(180\\degree-\\theta)"))
+
 ^{:nextjournal.clerk/visibility #{:hide}}
-(clerk/tex "\\cos(\\theta)=\\cos(\\theta+360\\degree)")
+(clerk/tex
+ (case trig-fn
+   "\\cos" "\\cos(\\theta)=\\cos(\\theta+360\\degree)"
+   "\\sin" "\\sin(\\theta)=\\sin(\\theta+360\\degree)"))
 
 (defn inverse-trig-fn [n]
   (case trig-fn
-   "\\cos" (Math/acos n)))
+    "\\cos" (Math/acos n)
+    "\\sin" (Math/asin n)))
 
 ;; Isolate the sinusoidal function
 
@@ -59,44 +65,53 @@
 ;; Now we can use the first identity to find the second solution within the interval:
 
 ^{:nextjournal.clerk/visibility #{:hide}}
-(clerk/tex (str "-" (deg (inverse-trig-fn (/ (- equals x-shift) amplitude))) " ^\\circ")) 
+(clerk/tex 
+ (case trig-fn
+   "\\cos" (str "-" (deg (inverse-trig-fn (/ (- equals x-shift) amplitude))) " ^\\circ")
+   "\\sin" (str "180^\\circ-" (deg (inverse-trig-fn (/ (- equals x-shift) amplitude))) " ^\\circ="
+                (- 180 (deg (inverse-trig-fn (/ (- equals x-shift) amplitude)))))))
 
 ;; Using the second identity, we can find all the solutions to our equation from the two angles we found above. The first solution gives us the following:
 
 ^{:nextjournal.clerk/visibility #{:hide}}
 (clerk/tex
- (str
-  "\\begin{aligned}"
-  period "x&="
+ (str "\\begin{aligned}" period "x&="
   (case mode
     :deg (str (deg (inverse-trig-fn (/ (- equals x-shift) amplitude))) "^\\circ"))
   "+n\\cdot360^\\circ \\\\\\\\"
   "x&=\\dfrac{" 
   (str (deg (inverse-trig-fn (/ (- equals x-shift) amplitude))) "^\\circ")
-  "+n\\cdot360^\\circ}{"
-  period
-  "}=" 
+  "+n\\cdot360^\\circ}{" period "}=" 
   (/ (Math/round (* 100 (/ (deg (inverse-trig-fn (/ (- equals x-shift) amplitude)))
-                           period))) 
-     100.0)
-  "^\\circ+n\\cdot72^\\circ\\end{aligned}"))
+                           period))) 100.0)
+  "^\\circ+n\\cdot"
+      (/ 360 period)
+      "^\\circ\\end{aligned}"))
 
 ;; Similarly, the second solution gives us the following:
 
 ^{:nextjournal.clerk/visibility #{:hide}}
 (clerk/tex
- (str
-  "\\begin{aligned}"
-  period "x&=-"
-  (case mode
-    :deg (str (deg (inverse-trig-fn (/ (- equals x-shift) amplitude))) "^\\circ"))
+ (str "\\begin{aligned}" period "x&="
+      (case trig-fn 
+        "\\cos"
+        (case mode
+          :deg (str (deg (inverse-trig-fn (/ (- equals x-shift) amplitude))) "^\\circ"))
+        "\\sin"
+        (case mode
+          :deg (str (- 180 (deg (inverse-trig-fn (/ (- equals x-shift) amplitude)))) "^\\circ")))
   "+n\\cdot360^\\circ \\\\\\\\"
-  "x&=\\dfrac{-"
+  "x&=\\dfrac{"
+       (case trig-fn 
+        "\\cos"
   (str (deg (inverse-trig-fn (/ (- equals x-shift) amplitude))) "^\\circ")
-  "+n\\cdot360^\\circ}{"
-  period
-  "}=-"
-  (/ (Math/round (* 100 (/ (deg (inverse-trig-fn (/ (- equals x-shift) amplitude)))
-                           period)))
-     100.0)
-  "^\\circ+n\\cdot72^\\circ\\end{aligned}"))
+         "\\sin"
+          (str (- 180 (deg (inverse-trig-fn (/ (- equals x-shift) amplitude)))) "^\\circ"))
+  "+n\\cdot360^\\circ}{" period "}="
+      (case mode
+        :deg
+        (/ (- 180 (deg (inverse-trig-fn (/ (- equals x-shift) amplitude))))
+                          period))
+  "^\\circ+n\\cdot"
+      (/ 360 period)
+      "^\\circ\\end{aligned}"))
